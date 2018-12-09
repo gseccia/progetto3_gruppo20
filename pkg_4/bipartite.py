c045b413d07a22849fd8b49769be271494ffc388
gseccia <giuseppeseccia1997@gmail.com>
2018-12-09 17:11:31 +0100

    specified methods signature.

---
+++ pkg_4/bipartite.py
@@ -0,0 +1,6 @@
+from typing import Tuple, List, Optional
+from TdP_collections.graphs.graph import Graph
+
+
+def bipartite(G: Graph) -> Optional[Tuple[List[Graph.Vertex], List[Graph.Vertex]]]:
+    pass
