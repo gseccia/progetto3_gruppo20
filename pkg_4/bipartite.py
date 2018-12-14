ace33c5b79358cd389c62da8a5de7c42d766d36d
gseccia <giuseppeseccia1997@gmail.com>
2018-12-14 21:07:06 +0100

    bipartite updated

--- pkg_4/bipartite.py
+++ pkg_4/bipartite.py
@@ -1,6 +1,5 @@
-from typing import Tuple, Dict, List, Optional
+from typing import Tuple, Set, List, Optional
 from TdP_collections.graphs.graph import Graph
-
 
 """
     Scrivere	una	funzione	bipartite() che,	preso	in	input	un	grafo	G	non	diretto,	verifica	
@@ -11,78 +10,41 @@ from TdP_collections.graphs.graph import Graph
 """
 
 
-def bipartite_connected(G: Graph, src: Graph.Vertex, colorArr: Dict) -> Optional[Tuple[List[Graph.Vertex], List[Graph.Vertex]]]:
+def bipartite(g: Graph) -> Optional[Tuple[List[Graph.Vertex], List[Graph.Vertex]]]:
+    visited = set()  # Insieme dei vertici visitati
+    x = []           # init partizione 1
+    y = []           # init partizione 2
+    for u in g.vertices():  # per ogni vertice del grafo
+        if u not in visited:  # che non ho ancora visitato
+            visited.add(u)  # lo segno come visitato
+            result = bipartite_connected(g, u, visited)  # calcolo bipartite della componente connessa del grafo
+                                                         # in cui si trova il vertice
+            if result is not None:   # se la componente connessa è bipartibile
+                x = x + result[0]    # aggiorno la partizione 1
+                y = y + result[1]    # aggiorno la partizione 2
+            else:   # altrimenti
+                return None          # il grafo non è bipartibile
+    return x, y                      # restituisco le partizioni
 
-    # Create a color array to store colors
-    # assigned to all veritces. Vertex
-    # number is used as index in this array.
-    # The value '-1' of colorArr[i] is used to
-    # indicate that no color is assigned to
-    # vertex 'i'. The value 1 is used to indicate
-    # first color is assigned and value 0
-    # indicates second color is assigned.
-    queue = []
-    X = []  # partition with color 0
-    Y = []  # partition with color 1
 
-    queue.append(src)
-    X.append(src)
-
-    # Run while there are vertices in queue
-    # (Similar to BFS)
-    while queue:
-
-        u = queue.pop()
-
-        for v in G.incident_edges(u):
-            d = v.opposite(u)
-            # An edge from u to v exists and destination
-            # v is not colored
-            if colorArr[d] == -1:
-
-                # Assign alternate color to this
-                # adjacent v of u
-                color = 1 - colorArr[u]
-                colorArr[d] = color
-                if color == 1:
-                    X.append(d)
-                elif color == 0:
-                    Y.append(d)
-                queue.append(d)
-
-            # An edge from u to v exists and destination
-            # v is colored with same color as u
-            elif colorArr[d] == colorArr[u]:
-                return None
-
-    # If we reach here, then all adjacent
-    # vertices can be colored with alternate
-    # color
-    return X, Y
-
-def bipartite(G: Graph) -> Optional[Tuple[List[Graph.Vertex], List[Graph.Vertex]]]:
-  """Perform BFS for entire graph and return forest as a dictionary.
-
-  Result maps each vertex v to the edge that was used to discover it.
-  (vertices that are roots of a BFS tree are mapped to None).
-  """
-  colorArr = {}
-  X=[]
-  Y=[]
-  for v in G.vertices():
-      colorArr[v] = -1
-
-  for u in colorArr:
-        if colorArr[u] == -1:            # u will be a root of a tree
-            colorArr[u] = 1
-            try:
-                X_connected,Y_connected = bipartite_connected(G, u, colorArr)
-            except:
-                return None
-            for elem in X_connected:
-                X.append(elem)
-                colorArr[elem]=1
-            for elem in Y_connected:
-                Y.append(elem)
-                colorArr[elem]=0
-  return X,Y
+def bipartite_connected(g: Graph, s: Graph.Vertex, discovered: Set[Graph.Vertex]) -> Optional[Tuple[List[Graph.Vertex], List[Graph.Vertex]]]:
+    colour = {s: 0}  # assegno al primo vertice colore 0
+    q = [s]          # creo una coda con i vertici da considerare
+    x = [s]          # creo la sotto-partizione 1 con assegnato il primo vertice
+    y = []           # creo la sotto- partizione 2
+    while q:
+        current = q.pop()  # prendo il vertice da elaborare
+        next_colour = 1 - colour[current]  # i vertici opposti avranno colore opposto al vertice corrente
+        for dest in g.incident_edges(current):  # considero tutti gli archi uscenti
+            v = dest.opposite(current)   # e mi prendo il vertice opposto
+            if v not in discovered:    # se è la prima volta che vedo quel vertice
+                discovered.add(v)      # lo segno come visitato
+                colour[v] = next_colour   # gli assegno il nuovo colore
+                if next_colour == 0:    # se 0
+                    x.append(v)    # lo assegno alla prima sotto-partizione
+                else:    # altrimenti
+                    y.append(v)   # lo assegno alla seconda sotto-partizione
+                q.append(v)  # il vertice visitato dovrà essere considerato al prossimo ciclo
+            elif colour[v] != next_colour:  # altrimenti se ho già visto il vertice e il suo colore non è opposto al vertice corrente
+                return None   # la componente connessa al vertice sorgente non è bipartibile
+    return x, y  # se arrivo qui vuol dire che è bipartibile e restituisco le partizioni
