5fab9c4fffa6f3c9f6ee2d94fd017ea622860994
gseccia <giuseppeseccia1997@gmail.com>
2018-12-27 15:59:49 +0100

    refactored

--- pkg_1/list_routes.py
+++ pkg_1/list_routes.py
@@ -14,82 +14,36 @@ from airports_time_schedule.ATS import *
     
 """
 
-"""
-def list_routes(airports: List[Airport], flights: List[Flight], start: Airport, end: Airport, t_start: datetime, T: timedelta) ->Optional[List[List[Flight]]]:
-    # Table to be filled up using DP.
-    # The value count[i][j][e] will / store
-    # count of possible walks from i to
-    # j in e minutes, the possible walks in a list of list of flights, for each walk the time remaining
-    paths = dict()
-    for src in airports:    # n*n
-        paths[src]=dict()
-        for dest in airports:
-            paths[src][dest] = dict()
 
-    for flight in flights:   # m
-        if l(flight) >= t_start and a(flight)-l(flight) <= T:
-            paths[s(flight)][d(flight)][a(flight) - l(flight)] = [[flight]]
-    i=0
-    path_added = True
-    while path_added:  # T/flight_min *
-        path_added = False
-        for flight in flights:   # m *
-            for src in paths:      # n *
-                for possible_duration_time in paths[src][s(flight)]:  # T/flight_min *
-                    if l(flight) >= t_start + possible_duration_time+c(s(flight)):
-                        flight_cost = calc_weight(flight, s(flight), t_start + possible_duration_time)
-                        path_relative_duration = possible_duration_time + flight_cost
-                        if path_relative_duration <= T:
-                            for path in paths[src][s(flight)][possible_duration_time]:  # boh *
-                                    i+=1
-                                    new_path_discovered = path + [flight]
-                                    if path_relative_duration not in paths[src][d(flight)]:  # T/t_flight_min
-                                        path_added = True
-                                        paths[src][d(flight)][path_relative_duration] = [new_path_discovered]
-                                    elif new_path_discovered not in paths[src][d(flight)][path_relative_duration]: # boh
-                                        path_added = True
-                                        paths[src][d(flight)][path_relative_duration].append(new_path_discovered)
-
-    #print possible paths
-    print("path visited ",i)
-    routes = []
-    for time_valid in paths[start][end]:
-        for flight_path in paths[start][end][time_valid]:
-            routes.append(flight_path)
-    return None if len(routes) is 0 else routes
-"""
-
-
-def list_routes(flights: List[Flight], start: Airport, b: Airport, t, T: timedelta):
-    current_path = [timedelta(0, 0), []]
+def list_routes(flights: List[Flight], start: Airport, b: Airport, t, T: timedelta) -> Optional[List[List[Flight]]]:
+    current_path_duration = timedelta(0, 0)
+    current_path = []
     paths = []
 
-    # Construction
-    available_flight = dict()
+    available_flight_per_airport = dict()
     for flight in flights:
         if l(flight) >= t and a(flight)-l(flight) <= T:
-            if s(flight) in available_flight:
-                available_flight[s(flight)].append(flight)
+            if s(flight) in available_flight_per_airport:
+                available_flight_per_airport[s(flight)].append(flight)
             else:
-                available_flight[s(flight)] = [flight]
+                available_flight_per_airport[s(flight)] = [flight]
 
-    list_routes_rec(available_flight, start, b, t, T, current_path, paths)
-    return paths
+    find_possible_routes(available_flight_per_airport, start, b, t, T,current_path_duration, current_path, paths)
+    return paths if len(paths) is not 0 else None
 
 
-def list_routes_rec(flights: Dict, start: Airport, b: Airport, arrival_time: datetime, T: timedelta, current_path: List, paths: List):
+def find_possible_routes(flights: Dict, start: Airport, b: Airport, arrival_time: datetime, T: timedelta,
+                         current_path_duration: timedelta, current_path: List, paths: List) -> None:
     if start == b:
-        paths.append(current_path[1].copy())
+        paths.append(current_path.copy())
     else:
         for flight in flights[start]:
-            actual_cost = calc_weight(flight, s(flight), arrival_time)
-            cost = current_path[0] + actual_cost
+            relative_cost = calc_weight(flight, s(flight), arrival_time)
+            cost = current_path_duration + relative_cost
             if cost < T and arrival_time+c(start) <= l(flight):
-                current_path[0] += actual_cost
-                current_path[1].append(flight)
-                list_routes_rec(flights, d(flight), b, a(flight), T, current_path, paths)
-                current_path[1].remove(flight)
-                current_path[0] -= actual_cost
+                new_path_duration = current_path_duration + relative_cost
+                new_path = current_path + [flight]
+                find_possible_routes(flights, d(flight), b, a(flight), T,new_path_duration, new_path, paths)
 
 
 def calc_weight(e: Flight, u: Airport, t: datetime) -> timedelta:
--- pkg_1/test_list_routes.py
+++ pkg_1/test_list_routes.py
@@ -3,7 +3,7 @@ from airports_time_schedule.ATS import *
 from pkg_3.random_test import *
 
 airports,flights = read_time_schedule_from_files("../airports_time_schedule/airports.csv", "../airports_time_schedule/flights.csv")
-"""
+
 start=airports[0]
 end=airports[5]
 print("start ",start)
@@ -14,15 +14,4 @@ if r is not None:
         print(elem)
 else:
     print("empty path")
-"""
 
-tmp_flight = list()
-for i in range(2):
-    tmp_flight.append(get_random_flight())
-
-r = list_routes(tmp_flight,s(tmp_flight[0]), d(tmp_flight[0]),datetime.strptime("00:00","%H:%M"), timedelta(hours=24,minutes=0))
-if len(r) is not 0:
-    for elem in r:
-        print(elem)
-else:
-    print("empty path")
